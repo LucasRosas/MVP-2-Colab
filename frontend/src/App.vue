1d78ca7c5093983264baf96268cdfe2b95eafccb
Lucas Rosas <lucasrosas@outlook.com>
2025-07-05 09:21:43 -0300

    finalização das classificacoes e integracao com api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,140 +2,169 @@
   <main class="container">
     <div class="header">
       <h1>
-        Posso comer esse cogumelo?
+        Posso comer esse cogumelo? 🍄
       </h1>
       <p>
-        Escolha as características do cogumelo e descubra se ele é comestível ou não.
+        Marque as características do seu cogumelo e descubra se ele é comestível 😋 ou venenoso 💀.
       </p>
     </div>
     <div class="itens" :style="{ transform: `translateX(calc(-${controls * 100}% - ${controls}*20px))` }">
       <div class="item">
         Formato do chapéu
         <div class="options">
-          <div class="option" :class="{ active: output['cap-shape'] == i.value }" v-for="i in formats" :key="i.name"
-            :style="{ backgroundImage: `url(./${i.name}.png)` }" @click="output['cap-shape'] = i.value">
+          <div class="option" :class="{ active: output['capShape'] == i.value }" v-for="i in formats" :key="i.name"
+            :style="{ backgroundImage: `url(./${i.name}.png)` }" @click="select('capShape', i.value)"><span>
+              {{ i.desc }}
+            </span>
           </div>
         </div>
       </div>
       <div class="item">
-        Textura da superfície<div class="options">
-          <div class="option" :class="{ active: output['cap-surface'] == i.value }" v-for="i in surfaces" :key="i.name"
-            :style="{ backgroundImage: `url(./${i.name}.png)` }" @click="output['cap-surface'] = i.value">
+        Textura da superfície
+        <div class="options">
+          <div class="option" :class="{ active: output['capSurface'] == i.value }" v-for="i in surfaces" :key="i.name"
+            :style="{ backgroundImage: `url(./${i.name}.png)` }" @click="select('capSurface', i.value)"><span>
+              {{ i.desc }}
+            </span>
           </div>
         </div>
       </div>
       <div class="item">
-        Cor do chapéu<div class="options">
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
+        Cor do chapéu
+        <div class="options">
+          <div class="option colors" :class="{ active: output['capColor'] == i.value }" v-for="i in colors"
+            :key="i.name" :style="{ backgroundImage: `url(./filter.png)`, backgroundColor: i.name, }"
+            @click="select('capColor', i.value)">
+            <span>
+              {{ i.desc }}
+            </span>
+          </div>
         </div>
       </div>
       <div class="item">
-        Presença de manchas<div class="options">
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-
+        Presença de manchas<div class="options bruises">
+          <div class="option" :class="{ active: output['bruises'] == i.value }" v-for="i in bruises" :key="i.name"
+            :style="{ backgroundImage: `url(./${i.name}.png)` }" @click="select('bruises', i.value)">
+            <span>
+              {{ i.desc }}
+            </span>
+          </div>
         </div>
       </div>
       <div class="item">
         Odor característico<div class="options">
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
-          <div class="option bell"></div>
+          <div class="option" :class="{ active: output['odor'] == i.value }" v-for="i in odors" :key="i.name"
+            :style="{ backgroundImage: `url(./${i.name}.png)` }" @click="select('odor', i.value)">
+            <span>
+              {{ i.desc }}
+            </span>
+          </div>
         </div>
       </div>
     </div>
     <div class="between">
-      <button class="button" @click="previous">
+      <button class="button" :disabled="controls == 0" @click="previous">
         Anterior
       </button>
-      <button class="button" @click="next">
+      <button class="button" :disabled="controls == 4" @click="next">
         Próximo
       </button>
     </div>
 
-    <div class="result">
+    <div class="result" v-if="p != null">
       <h1>
         Resultado:
       </h1>
-      <div class="between">
-        <div>Venenoso</div>
-        <div>Comestível</div>
+      <div class="barr">
+        <div class="between">
+          <label>Comestível</label>
+          <label>Venenoso
+          </label>
+        </div>
+        <div class="ind" :style="{ left: `${(p * 100).toFixed(0)}%` }"></div>
       </div>
-      <div class="barr"></div>
       <div class="resultado">
-        <template v-if="p != null">
-
-          <div v-if="p == 0">
-            O cogumelo selecionado é <strong>comestível</strong>! Aproveite sua refeição!
-          </div>
-          <div v-else-if="p < 1">
-            Há uma chance de <strong>{{ (p * 100).toFixed(0) }}%</strong> do cogumelo ser venenoso. É melhor não
-            arriscar!
-          </div>
-          <div v-else>
-            O cogumelo selecionado é <strong>venenoso</strong>. Evite consumi-lo!
-          </div>
-        </template>
+        <div v-if="p == 0">
+          O cogumelo selecionado é <strong>comestível</strong>! Aproveite sua refeição! 😋
+        </div>
+        <div v-else-if="p < 1">
+          Há <strong>{{ (p * 100).toFixed(0) }}%</strong> de chance do seu cogumelo ser <strong>venenoso 💀</strong>.
+          <br />É
+          melhor não
+          arriscar!
+        </div>
+        <div v-else>
+          O seu cogumelo é <strong>venenoso</strong> 💀. Não coma!
+        </div>
       </div>
 
+    </div>
+    <div class="result" v-else>
+      Marque as características, o resultado vai aparecer aqui.
     </div>
   </main>
 </template>
 
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const output = ref({
-  'cap-shape': null,
-  'cap-surface': null,
-  'cap-color': null,
+  'capShape': null,
+  'capSurface': null,
+  'capColor': null,
   bruises: null,
   odor: null
 });
 
 const formats = ref([
-  { name: 'bell', value: 'b' },
-  { name: 'conical', value: 'c' },
-  { name: 'convex', value: 'x' },
-  { name: 'flat', value: 'f' },
-  { name: 'knobbed', value: 'k' },
-  { name: 'sunken', value: 's' }
+  { name: 'bell', value: 'b', desc: 'sino' },
+  { name: 'conical', value: 'c', desc: 'cone' },
+  { name: 'convex', value: 'x', desc: 'convexo' },
+  { name: 'flat', value: 'f', desc: 'achatado' },
+  { name: 'knobbed', value: 'k', desc: 'protuberante' },
+  { name: 'sunken', value: 's', desc: 'afundado' }
 ]);
 const surfaces = ref([
-  { name: 'fibrous', value: 'f' },
-  { name: 'grooves', value: 'g' },
-  { name: 'scaly', value: 'y' },
-  { name: 'smooth', value: 's' },
+  { name: 'fibrous', value: 'f', desc: 'fibroso' },
+  { name: 'grooves', value: 'g', desc: 'sulcada' },
+  { name: 'scaly', value: 'y', desc: 'escamosa' },
+  { name: 'smooth', value: 's', desc: 'lisa' },
 ]);
 
 const colors = ref([
   { name: 'brown', value: 'n' },
-  { name: 'buff', value: 'b' },
-  { name: 'cinnamon', value: 'c' },
+  { name: '#DAA06D', value: 'b' },
+  { name: '#D47E30', value: 'c' },
   { name: 'gray', value: 'g' },
   { name: 'green', value: 'r' },
   { name: 'pink', value: 'p' },
   { name: 'purple', value: 'u' },
   { name: 'red', value: 'e' },
   { name: 'white', value: 'w' },
-  { name: 'yellow', value: 'y' }
+  { name: '#FBEC5D', value: 'y' }
 ]);
+
+const bruises = ref([
+  { name: 'knobbed', value: 't' },
+  { name: 'convex', value: 'f' }
+]);
+
+const odors = ref([
+  { value: 'a', name: 'almond', desc: 'amêndoa' },
+  { value: 'l', name: 'anise', desc: 'anis' },
+  { value: 'c', name: 'creosote', desc: 'alcatrão' },
+  { value: 'y', name: 'fishy', desc: 'peixe' },
+  { value: 'f', name: 'foul', desc: 'fétido' },
+  { value: 'm', name: 'musty', desc: 'mofo' },
+  { value: 'n', name: 'none', desc: 'nenhum' },
+  { value: 'p', name: 'pungent', desc: 'ardido' },
+  { value: 's', name: 'spicy', desc: 'especiado' },
+]);
+
+const select = (key, value) => {
+  output.value[key] = value;
+}
 
 const controls = ref((0))
 
@@ -151,7 +180,31 @@ const previous = () => {
   }
 };
 
-const p = ref(0.5);
+const p = ref(null);
+const loading = ref(false)
+
+watch(() => output.value, async () => {
+
+  const all = Object.values(output.value).every(i => i)
+  if (!all) return
+  loading.value = true
+
+  let params = new URLSearchParams()
+  Object.entries(output.value).forEach(([k, v]) => {
+    params.set(k, v)
+  })
+  try {
+    const res = await fetch(`http://localhost:5000/classify?${params}`, {
+      method: 'GET'
+    })
+    const value = await res.json()
+
+    p.value = value.result.p
+    loading.value = false
+  } catch (error) {
+
+  }
+}, { deep: true })
 </script>
 
 <style>
@@ -160,6 +213,26 @@ const p = ref(0.5);
 
 * {
   box-sizing: border-box;
+}
+
+h1 {
+  margin-block: 1rem;
+  font-size: 3rem;
+}
+
+label {
+  font-size: 1.5rem;
+  padding-inline: 1rem;
+  margin-top: -0.5rem;
+  z-index: 1;
+  font-family: 'Pixelify Sans', sans-serif;
+  color: #0b4018;
+  font-weight: 800;
+
+  &:first-of-type {
+    color: white;
+  }
+
 }
 
 body {
@@ -199,7 +272,7 @@ main.container {
   flex-direction: column;
   margin-inline-start: 50vw;
   transform: translateX(-50%);
-  width: 1000px;
+  width: 1150px;
 }
 
 
@@ -208,12 +281,13 @@ main.container {
   transition: all 0.3s ease-in-out;
   display: flex;
   align-items: center;
-  margin-top: 20px;
-  max-width: 1000px;
+  max-width: 1150px;
   gap: 20px;
 }
 
 .item {
+  height: -webkit-fill-available;
+  height: -moz-available;
   background-color: rgba(0, 0, 0, 0.123);
   border: 0.1px solid rgba(36, 36, 36, 0.2);
   border-top: 0.1px solid rgba(255, 255, 255, 0.2);
@@ -247,19 +321,31 @@ main.container {
   width: 200px;
   height: 200px;
   cursor: pointer;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+
+  &.colors {
+    width: 150px;
+    background-blend-mode: multiply;
+    height: 150px;
+  }
 
   &.active {
     outline: 4px solid #d5d5d5;
     outline-offset: 4px;
   }
-}
 
-.conical {
-  background-image: url('./conical.png');
-}
+  span {
+    background-image: url('./filter.png');
+    background-color: #0b4018;
+    background-blend-mode: multiply;
+    background-size: cover;
+    background-position: center;
+    color: rgb(203, 202, 201);
+    padding: 0.25rem 0.5rem;
 
-.bell {
-  background-image: url('./bell.png');
+  }
 }
 
 .between {
@@ -270,15 +356,16 @@ main.container {
 
 .barr {
   background: #1cff14;
-  background: linear-gradient(90deg, rgba(28, 255, 20, 1) 0%, rgba(87, 199, 133, 1) 32%, rgba(69, 100, 255, 1) 100%);
+  background: linear-gradient(90deg, rgba(69, 100, 255, 1) 0%, rgba(87, 199, 133, 1) 32%, rgba(28, 255, 20, 1) 100%);
   height: 20px;
   position: relative;
+  margin: 40px;
 
   &:before,
   &:after {
     content: '';
     background: #1cff14;
-    background: linear-gradient(90deg, rgba(28, 255, 20, 1) 0%, rgba(87, 199, 133, 1) 32%, rgba(69, 100, 255, 1) 100%);
+    background: linear-gradient(90deg, rgba(69, 100, 255, 1) 0%, rgba(87, 199, 133, 1) 32%, rgba(28, 255, 20, 1) 100%);
     height: 10px;
     width: calc(100% - 20px);
     position: absolute;
@@ -292,10 +379,26 @@ main.container {
   &:after {
     top: 20px;
   }
+
+  .ind {
+    position: absolute;
+    top: -40px;
+    margin-left: -30px;
+    width: 100px;
+    height: 100px;
+    background-image: url('./bell.png');
+    transition: all 0.3s ease-in-out;
+    background-size: cover;
+    background-position: center;
+    z-index: 1;
+  }
 }
 
 .resultado {
   margin-top: 1rem;
+  margin-bottom: 4rem;
   min-height: 2rem;
+  text-align: center;
+  font-size: 2rem;
 }
 </style>
